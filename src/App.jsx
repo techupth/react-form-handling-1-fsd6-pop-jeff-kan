0d9b35032822d161a743d4d72821a4c11e03ebb9
Kan <kanalovefootball@gmail.com>
2024-06-05 13:50:55 +0700

    feat:Change input greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [changeMessage, setMessage] = useState("");
+
+  const [showMessage, setShow] = useState("Greeting Message");
+  const handleMessageClick = () => {
+    setShow(changeMessage);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{showMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={changeMessage}
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleMessageClick}>Update text</button>
       </div>
     </div>
   );
