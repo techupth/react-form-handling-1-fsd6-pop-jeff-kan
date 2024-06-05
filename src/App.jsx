cdfdab5e8573942dac7f3de7f5bce2d2628492f2
Pop <shounen.pop@gmail.com>
2024-06-05 11:39:17 +0700

    add: usetate onclick function

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("");
+  const [showMessage, setShowMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{showMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => setGreetingMessage(event.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={() => setShowMessage(greetingMessage)}>
+          Update text
+        </button>
       </div>
     </div>
   );
